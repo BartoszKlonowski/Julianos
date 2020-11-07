6b426f2777d098e513e5ede57305213e655300ac
Bartosz Klonowski <Bartosz-Klonowski@tlen.pl>
2020-11-07 23:30:57 +0100

    Implement RelayCommand to handle Commands in application

---
+++ DesktopApp/ViewModels/Command/Command.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Input;
+
+
+namespace DesktopApp.ViewModels.Command
+{
+    public class Command : ICommand
+    {
+        public event EventHandler CanExecuteChanged = ( sender, e ) => { };
+
+
+        public Command( Action action )
+        {
+            actionToExecute = action;
+        }
+
+
+        public bool CanExecute( object parameter ) => true;
+
+
+        public void Execute( object parameter )
+        {
+            actionToExecute();
+        }
+
+
+        private Action actionToExecute;
+    }
+}
