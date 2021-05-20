9de2dc5b56ca0c04b8ca6b7c37b01ec6df3fb3e9
Bartosz Klonowski <Bartosz-Klonowski@tlen.pl>
2021-05-20 03:10:13 +0200

    Implement basic navigation mechanism

---
+++ DesktopApp/View/Screens/AppContent.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+
+namespace DesktopApp.View.Screens
+{
+    /// <summary>
+    /// Interaction logic for AppContent.xaml
+    /// </summary>
+    public partial class AppContent : UserControl
+    {
+        public AppContent()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DesktopApp/ViewModels/NavigationViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace DesktopApp.ViewModels
+{
+    public class NavigationViewModel : INotifyPropertyChanged
+    {
+        public NavigationViewModel()
+        {
+            View1 = new Command.Command(SetView1);
+            View2 = new Command.Command(SetView2);
+        }
+
+
+        private void SetView1()
+        {
+            AppContent = new View.Screens.AppContent();
+        }
+
+        private void SetView2()
+        {
+        }
+
+        public object AppContent
+        {
+            get => appContent;
+            set
+            {
+                appContent = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ViewModels.Command.Command View1
+        { get; }
+
+        public ViewModels.Command.Command View2
+        { get; }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+        private object appContent;
+    }
+}
