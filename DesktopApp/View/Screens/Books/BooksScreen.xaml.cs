49ce1be6f5c75906ab1415080254f68bafc47f01
Bartosz Klonowski <70535775+BartoszKlonowski@users.noreply.github.com>
2021-06-08 14:15:04 +0200

    Books screen - main navigation & widgets (#8)
    * Add empty BooksScreen UserControl
    
    * Move BooksScreen to Books subdirectory
    
    The Books screen will contain several UserControls, so to keep it clean
    and separated from other screens it has been moved to separate directory:
    View/Screens/Books/
    where all the UserControls related to the Books screns will be kept.
    
    * Add BooksListViewTemplate to the Styles resources
    
    The BooksListViewTemplate will be used to dynamically create the list of
    Books in the db storage. The template needs to specify the way how each
    element will look like and how will be rendered.
    
    NOTE: to simplify the codebase the new template has been placed in the
    same file as ModernButton. But as there are now more templates, this
    ResourceDictionary file has been renamed.
    
    * Add ListView for books
    
    * Implement BooksViewModel
    
    * Add opactiy animation to the book panel widget

--- DesktopApp/View/Screens/Books/BooksScreen.xaml.cs
+++ DesktopApp/View/Screens/Books/BooksScreen.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
+
 
 namespace DesktopApp.View.Screens
 {
@@ -23,8 +11,7 @@ namespace DesktopApp.View.Screens
         public BooksScreen()
         {
             InitializeComponent();
-            DAL.DatabaseContext dbContext = new DAL.DatabaseContext();
-            FilteredListView.ItemsSource = dbContext.Books.ToList();
+            DataContext = new ViewModels.BooksScreenViewModel();
         }
     }
 }
---
+++ DesktopApp/ViewModels/BooksScreenViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows.Controls;
+
+
+namespace DesktopApp.ViewModels
+{
+    public class BooksScreenViewModel : INotifyPropertyChanged
+    {
+        public BooksScreenViewModel()
+        {
+        }
+
+        public System.Collections.Generic.List<Models.BookModel> BooksList
+        {
+            get => new DAL.DatabaseContext().Books.ToList();
+        }
+
+        public UserControl BooksPanel
+        {
+            get => booksPanel;
+            set
+            {
+                booksPanel = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int SelectedBookIndex
+        {
+            get => selectedBookIndex;
+            set
+            {
+                selectedBookIndex = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged( [CallerMemberName] string name = null ) => PropertyChanged?.Invoke( this, new PropertyChangedEventArgs( name ) );
+
+        private UserControl booksPanel;
+        private int selectedBookIndex;
+    }
+}
