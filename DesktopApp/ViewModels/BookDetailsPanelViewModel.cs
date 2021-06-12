21a5096781058316bba590de4b521df2bc92d0e8
Bartosz Klonowski <Bartosz-Klonowski@tlen.pl>
2021-06-12 19:37:06 +0200

    Implement ViewModel of the selected book

---
+++ DesktopApp/ViewModels/BookDetailsPanelViewModel.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Linq;
+
+
+namespace DesktopApp.ViewModels
+{
+    public class BookDetailsPanelViewModel : INotifyPropertyChanged
+    {
+        public BookDetailsPanelViewModel( int selectedBookID )
+        {
+            selectedBook = new DAL.DatabaseContext().Books.First( book => book.ID == selectedBookID );
+        }
+
+
+        public string BookTitle
+        {
+            get => selectedBook.Name;
+            set
+            {
+                OnPropertyChanged();
+            }
+        }
+
+        public string BookAuthor
+        {
+            get => selectedBook.Author;
+            set
+            {
+                OnPropertyChanged();
+            }
+        }
+
+        public string BookUrl
+        {
+            get => selectedBook.Url;
+            set
+            {
+                OnPropertyChanged();
+            }
+        }
+
+        public string BookImageSource
+        {
+            get => selectedBook.Image;
+            set
+            {
+                OnPropertyChanged();
+            }
+        }
+
+        public decimal BookRating
+        {
+            get => selectedBook.Rating;
+            set
+            {
+                OnPropertyChanged();
+            }
+        }
+
+        public string BookPrize
+        {
+            get => $"{selectedBook.Prize/10} zł {selectedBook.Prize % 100} gr";
+            set
+            {
+                OnPropertyChanged();
+            }
+        }
+
+        public DateTime BookBuyDate
+        {
+            get => selectedBook.BuyingDate;
+            set
+            {
+                OnPropertyChanged();
+            }
+        }
+
+        public Command.Command SwitchToBooksScreenCommand
+        { get; }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged( [CallerMemberName] string name = null ) => PropertyChanged?.Invoke( this, new PropertyChangedEventArgs( name ) );
+
+
+        private Models.BookModel selectedBook;
+    }
+}
