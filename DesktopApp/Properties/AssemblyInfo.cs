4df1e2e498a618311b2e45caa15b9cbb09a682a4
Bartosz Klonowski <70535775+BartoszKlonowski@users.noreply.github.com>
2021-12-27 19:02:26 +0100

    New project name - Libris (#12)
    * Rename all Julianos occurrences into Libris
    
    * Rename all Hephaestus to Libris
    
    * Use Libris name in the Assembly info
    
    * Rename solution file to new Libris name
    
    * Rename CI config file into new name

--- DesktopApp/Properties/AssemblyInfo.cs
+++ DesktopApp/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle( "DesktopApp" )]
+[assembly: AssemblyTitle( "Libris" )]
 [assembly: AssemblyDescription( "" )]
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "" )]
-[assembly: AssemblyProduct( "DesktopApp" )]
-[assembly: AssemblyCopyright( "Copyright ©  2020" )]
+[assembly: AssemblyProduct( "Libris" )]
+[assembly: AssemblyCopyright( "Copyright ©Bartosz Klonowski 2022" )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
 
